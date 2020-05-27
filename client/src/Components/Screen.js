36e743c00932d3eea829a57d4102e603c9a44734
Shufan <shufanwen@Shufans-MacBook-Pro.local>
2020-05-27 00:08:39 -0500

    screen optimization

--- client/src/Components/Screen.js
+++ client/src/Components/Screen.js
@@ -3,23 +3,122 @@ import {Container,Card,Row,Col} from "reactstrap";
 
 class Screen extends React.Component {
 
+    constructor() {
+        super()
+        this.state = {
+            representation:"complex"
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.optimizeOutput = this.optimizeOutput.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({representation:event.target.value});
+    }
+
+    optimizeOutput(numObject,calcType) {
+        var unit = null;
+        var newNumber = null;
+        switch(calcType) {
+            case "L":
+                unit = "H"
+                break
+            case "C":
+                unit = "F"
+                break
+            default:
+                unit = "\u03A9"
+        }
+        if (calcType === "complex" || calcType === "Z") {
+            if(numObject.mag > 1000000) {
+                newNumber = {re:numObject.re/1000000, im:numObject.im/1000000, mag:numObject.mag/1000000}
+                return {number:newNumber, unit:unit, prefix:"M"}
+            } else if (numObject.mag > 1000) {
+                newNumber = {re:numObject.re/1000, im:numObject.im/1000, mag:numObject.mag/1000}
+                return {number:newNumber, unit:unit, prefix:"k"}
+            } else if (numObject.mag > 1) {
+                newNumber = {re:numObject.re, im:numObject.im, mag:numObject.mag}
+                return {number:newNumber, unit:unit, prefix:""}
+            } else if (numObject.mag > 0.001) {
+                newNumber = {re:numObject.re/0.001, im:numObject.im/0.001, mag:numObject.mag/0.001}
+                return {number:newNumber, unit:unit, prefix:"m"}
+            } else if (numObject.mag > 0.000001) {
+                newNumber = {re:numObject.re/0.000001, im:numObject.im/0.000001, mag:numObject.mag/0.000001}
+                return {number:newNumber, unit:unit, prefix:"\u00B5"}
+            } else {
+                newNumber = {re:numObject.re/0.000000001, im:numObject.im/0.000000001, mag:numObject.mag/0.000000001}
+                return {number:newNumber, unit:unit, prefix:"n"}
+            }
+        } else {
+            if(numObject.re > 1000000) {
+                return {number:numObject.re/1000000, unit:unit, prefix:"M"}
+            } else if (numObject.re > 1000) {
+                return {number:numObject.re/1000, unit:unit, prefix:"k"}
+            } else if (numObject.re > 1) {
+                return {number:numObject.re, unit:unit, prefix:""}
+            } else if (numObject.re > 0.001) {
+                return {number:numObject.re/0.001, unit:unit, prefix:"m"}
+            } else if (numObject.re > 0.000001) {
+                return {number:numObject.re/0.000001, unit:unit, prefix:"\u00B5"}
+            } else {
+                return {number:numObject.re/0.000000001, unit:unit, prefix:"n"}
+            }
+        }
+    }
+
     render() {
         var text = null;
-        if(this.props.display === 0){text =<h1>0</h1>}
+        var rep = null;
+        if(this.props.display === 0){text =<h3>0</h3>}
         else if(this.props.calcType === "Z" || this.props.calcType === "complex") {
-            console.log("hello")
-            const real = typeof(this.props.display.re)=="number"?this.props.display.re.toFixed(3):0;
-            const imaginary = typeof(this.props.display.im)==="number"?this.props.display.im.toFixed(3):0;
-            console.log(this.props.display);
-            text = <h1>{real} + {imaginary}j</h1>
-        }else {
-            const real = this.props.display.re===null?0:this.props.display.re.toFixed(3);
-            text = <h1>{real}</h1>
+            var combine = null;
+            var reMag = 0;
+            var imPhase = 0;
+            var prefix = null;
+            var unit = null;
+            var suffix = null;
+            if(this.state.representation === "phasor") {
+                if (typeof(this.props.display.mag)==="number") {
+                    const display = this.optimizeOutput(this.props.display,this.props.calcType);
+                    reMag = display.number.mag.toFixed(3);
+                    imPhase = this.props.display.phase.toFixed(3);
+                    unit = display.unit;
+                    prefix = display.prefix;
+                }
+                combine = "\u2220"
+                suffix = "\u00B0"
+            }else{
+                if (typeof(this.props.display.mag)==="number") {
+                    const display = this.optimizeOutput(this.props.display,this.props.calcType);
+                    reMag = display.number.re.toFixed(3);
+                    imPhase = display.number.im.toFixed(3);
+                    unit = display.unit;
+                    prefix = display.prefix;
+                }
+                combine = "+"
+                suffix = "j"
+            }
+            text = <h3>{reMag+combine+imPhase+suffix} {prefix+unit}</h3>
+            rep = (this.state.representation === "complex")?
+            <button onClick = {this.handleChange} value="phasor" className="btn btn-outline-danger btn-sm">{"\u2102"}</button>:
+            <button onClick = {this.handleChange} value="complex" className="btn btn-outline-danger btn-sm">{"\u0398"}</button>
+        }else{
+            if(this.props.display.re === null) {
+                text = <h3>0</h3>
+            }else {
+                const real = this.optimizeOutput(this.props.display,this.props.calcType);
+                text = <h3>{real.number.toFixed(3)} {real.prefix+real.unit}</h3>
+            }
         }
         return <Container className="body">
             <Card>
                 <Container>
                     <Row>
+                        <Col xs = "1" className ="screen text-left">
+                            <div className = "screenButton">
+                                {rep} 
+                            </div>
+                        </Col>
                         <Col className ="screen text-right">
                             {text}
                         </Col>
